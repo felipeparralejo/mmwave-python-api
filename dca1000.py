Added necessary signal int16 conversion List of additional changes: - Included Azimuth FFT, and Elevation FFT - Included Elevation-Range heatmap - Solved doppler bin computation mistake - Added azimuth and elevation number of bins as parameters

dca1000.py
@@ -3,7 +3,7 @@
 # MODIFICATION OF THE adc.py FILE FROM LIBRARY openradar by Felipe Parralejo
 # under Apache License, Version 2.0.
 # Changes are needed to automatically read configuration parameters from mmwave
-# Studio lua script.
+# Studio lua script and convert frames from two's complement to int16 format.
 #
 # =============================================================================
 # Copyright 2019 The OpenRadar Authors. All Rights Reserved.
@@ -175,7 +175,8 @@ class DCA1000:
             timeout (float): Time to wait for packet before moving on
 
         Returns:
-            Full frame as array if successful, else None
+            Full frame as array if successful converted to int16 format
+            to recover signs from two's complement, else None
 
         """
         # Configure
@@ -199,7 +200,8 @@ class DCA1000:
 
             if byte_count % BYTES_IN_FRAME_CLIPPED == 0:
                 self.lost_packets = PACKETS_IN_FRAME_CLIPPED - packets_read
-                return ret_frame
+                # conversion from two's complement
+                return np.frombuffer(ret_frame, dtype=np.int16)
 
             curr_idx = ((packet_num - 1) % PACKETS_IN_FRAME_CLIPPED)
             try:
@@ -303,6 +305,7 @@ class DCA1000:
         # Separate IQ data
         ret[0::2] = raw_frame[0::4] + 1j * raw_frame[2::4]
         ret[1::2] = raw_frame[1::4] + 1j * raw_frame[3::4]
+        # ret = raw_frame[0::2] + 1j * raw_frame[1::2]
         return ret.reshape((num_chirps, num_rx, num_samples))
 
     @staticmethod

fourier.py
@@ -7,7 +7,11 @@ Included functions:
     - Range FFT
     - Doppler FFT
     - Azimuth FFT
+    - Elevation FFT
     - Plot Range FFT
+    - Plot Doppler FFT
+    - Plot Azimuth FFT
+    - Plot Elevation FFT
     - Fink peaks in range signal
 
 """
@@ -43,13 +47,27 @@ def dopplerFFT(signal):
 
 
 def azimuthFFT(signal):
-    azimuth = np.fft.fft(signal)
+    azimuth = np.fft.fft(signal, n=PARAMS.NUM_AZIM_BINS)
     azimuth = np.fft.fftshift(azimuth)
 
-    bins = np.fft.fftfreq(signal.size)*PARAMS.AZIM_MAX
-    bins = np.fft.fftshift(bins)
+    bins = np.linspace(-PARAMS.NUM_AZIM_BINS//2,
+                       PARAMS.NUM_AZIM_BINS//2-1,
+                       PARAMS.NUM_AZIM_BINS)*2/PARAMS.NUM_AZIM_BINS
+    bins = np.arcsin(bins)*180/np.pi
 
     return azimuth, bins
+
+
+def elevationFFT(signal):
+    elevation = np.fft.fft(signal, n=PARAMS.NUM_ELEV_BINS)
+    elevation = np.fft.fftshift(elevation)
+
+    bins = np.linspace(-PARAMS.NUM_ELEV_BINS//2,
+                       PARAMS.NUM_ELEV_BINS//2-1,
+                       PARAMS.NUM_ELEV_BINS)*2/PARAMS.NUM_ELEV_BINS
+    bins = np.arcsin(bins)*180/np.pi
+
+    return elevation, bins
 
 
 def plotFFTrange(signal):
@@ -71,6 +89,42 @@ def plotFFTrange(signal):
     ax.set_xlabel('Range (m)')
     ax.set_ylabel('Reflected Power')
     ax.set_title('Interpreting a Single Chirp')
+    plt.show()
+
+
+def plotFFTdoppler(signal):
+    doppler, bins = dopplerFFT(signal)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(bins, np.abs(doppler))
+    ax.set_xlabel('Doppler (m/s)')
+    ax.set_ylabel('Reflected Power')
+    ax.set_title('Interpreting a Single Sample')
+    plt.show()
+
+
+def plotFFTazimuth(signal):
+    azimuth, bins = azimuthFFT(signal)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(bins, np.abs(azimuth))
+    ax.set_xlabel('Azimuth (°)')
+    ax.set_ylabel('Reflected Power')
+    ax.set_title('Interpreting a Chirps Avg.')
+    plt.show()
+
+
+def plotFFTelevation(signal):
+    elevation, bins = elevationFFT(signal)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(bins, np.abs(elevation))
+    ax.set_xlabel('Elevation (°)')
+    ax.set_ylabel('Reflected Power')
+    ax.set_title('Interpreting a Chirps Avg.')
     plt.show()
 
 

heatmaps.py
@@ -7,6 +7,7 @@ Included functions:
     - Range Heatmap
     - Doppler-Range Heatmap
     - Azimuth-Range Heatmap
+    - Elevation-Range Heatmap
 
 """
 
@@ -16,13 +17,14 @@ from matplotlib.animation import FuncAnimation
 from dca1000 import DCA1000
 
 from params import PARAMS
-from fourier import rangeFFT, dopplerFFT, azimuthFFT
+from fourier import rangeFFT, dopplerFFT, azimuthFFT, elevationFFT
 
 
 def generateRangeHeatmap(raw_data):
     # Average antennas
     avg = np.mean(raw_data, axis=1)
 
+    # Only keeping positive ranges
     matrix = np.zeros((avg.shape[0], avg.shape[1]//2), dtype=complex)
 
     # Do FFT along each chirp's samples (range)
@@ -60,7 +62,7 @@ def generateAzimuthRangeHeatmap(raw_data):
 
     avg = np.mean(raw_data[:, :8, :], axis=0)
 
-    matrix = np.zeros((avg.shape[0], avg.shape[1]//2), dtype=complex)
+    matrix = np.zeros((PARAMS.NUM_AZIM_BINS, avg.shape[1]//2), dtype=complex)
 
     # Do FFT along each chirp's samples (range)
     for i in range(avg.shape[0]):
@@ -70,7 +72,34 @@ def generateAzimuthRangeHeatmap(raw_data):
     for i in range(avg.shape[1]//2):
         matrix[:, i], azimuth_bins = azimuthFFT(matrix[:, i])
 
-    return range_bins, azimuth_bins, matrix
+    # Angular resolution is not at > 75 degrees, we remove the first
+    # bin to remove -90 degrees and keep the rest
+    return range_bins, azimuth_bins[1:], matrix[1:]
+
+
+def generateElevationRangeHeatmap(raw_data):
+    # TX emissions are in the order TX1->TX3->TX2. For elevation,
+    # chirps from TX1 and TX3 can be averaged and concatenated with
+    # chirp from TX2.
+    # Then for each chirp, the first 8 VX antennas are for azimuth
+    # and the mean of all these with the remainder 9, 10 11 and 12 VX
+    # antennas are for elevation
+
+    avg = np.mean(raw_data, axis=0)
+
+    avg = np.concatenate([(avg[:4] + avg[4:8])/2, avg[8:]])
+
+    matrix = np.zeros((PARAMS.NUM_ELEV_BINS, avg.shape[1]//2), dtype=complex)
+
+    # Do FFT along each chirp's samples (range)
+    for i in range(avg.shape[0]):
+        matrix[i, :], range_bins = rangeFFT(avg[i])
+
+    # Do FFT along antennas (elevation)
+    for i in range(avg.shape[1]//2):
+        matrix[:, i], elevation_bins = elevationFFT(matrix[:, i])
+
+    return range_bins, elevation_bins[1:], matrix[1:]
 
 
 def plotRangeHeatmap(signal):
@@ -126,6 +155,27 @@ def plotAzimuthRangeHeatmap(raw_data):
     ax.set_title('Azimuth-Range Heatmap')
     ax.set_xlabel('Range (m)')
     ax.set_ylabel('Azimuth (°)')
+
+    plt.show()
+
+    return matrix
+
+
+def plotElevationRangeHeatmap(raw_data):
+    '''
+    Plot Elevation-Range heatmap
+    '''
+
+    range_bins, elevation_bins, matrix = generateElevationRangeHeatmap(
+        raw_data)
+
+    fig, ax = plt.subplots()
+
+    c = ax.pcolormesh(range_bins, elevation_bins, np.abs(matrix))
+    fig.colorbar(c, ax=ax)
+    ax.set_title('Elevation-Range Heatmap')
+    ax.set_xlabel('Range (m)')
+    ax.set_ylabel('Elevation (°)')
 
     plt.show()
 

live_plots.py
@@ -20,6 +20,10 @@ from params import PARAMS
 from fourier import rangeFFT, findPeaks
 from heatmaps import generateRangeHeatmap, generateAzimuthRangeHeatmap
 
+Y_MAX = 4e4
+Y_PEAK_TH = 1e4
+V_MAX = 1e4
+
 
 def liveRangePreview(dca):
     '''
@@ -27,7 +31,7 @@ def liveRangePreview(dca):
     '''
     # Figure
     fig = plt.figure()
-    ax = plt.axes(xlim=(0, PARAMS.R_MAX/2), ylim=(0, 5e6))
+    ax = plt.axes(xlim=(0, PARAMS.R_MAX/2), ylim=(0, Y_MAX))
     line = ax.plot([], [], 'b-8')[0]
 
     ax.grid()
@@ -53,7 +57,7 @@ def liveRangePreview(dca):
         adc_data = DCA1000.separate_tx(adc_data, num_tx=PARAMS.TX_ANTENNAS)
         range, bins = rangeFFT(adc_data[0, 0])
 
-        peaks = findPeaks(range, th=3.0e6)
+        peaks = findPeaks(range, th=Y_PEAK_TH)
 
         line.set_xdata(bins)
         line.set_ydata(np.abs(range))
@@ -86,7 +90,7 @@ def liveRangeHeatmapPreview(dca):
     # Figure
     fig, ax = plt.subplots()
     cax = ax.pcolormesh(bins, chirps,
-                        np.abs(matrix), vmin=0, vmax=1e6)
+                        np.abs(matrix), vmin=0, vmax=V_MAX)
     fig.colorbar(cax, ax=ax)
 
     ax.grid()
@@ -118,7 +122,7 @@ def liveAzimuthRangePreview(dca):
     # Figure
     fig, ax = plt.subplots()
     cax = ax.pcolormesh(range_bins, azimuth_bins,
-                        np.abs(matrix), vmin=0, vmax=1e6)
+                        np.abs(matrix), vmin=0, vmax=V_MAX)
     fig.colorbar(cax, ax=ax)
 
     ax.grid()

read_data.py
@@ -12,11 +12,12 @@ PARAMS to use radar configuration from the saved data.
 
 from params import PARAMS
 import pickle
+import numpy as np
 import heatmaps
-from fourier import plotFFTrange
+from fourier import plotFFTrange, plotFFTazimuth, plotFFTelevation
 from dca1000 import DCA1000
 
-data = pickle.load(open('data\openradar_14-12-22_MOAA5.dat', 'rb'))
+data = pickle.load(open('data\openradar_10-01-23_STFFD.dat', 'rb'))
 
 frames = data['data']
 config = data['config']
@@ -29,6 +30,11 @@ adc_data = DCA1000.organize(
 adc_data = DCA1000.separate_tx(adc_data, num_tx=PARAMS.TX_ANTENNAS)
 
 # plotFFTrange(adc_data[0, 0])
+# plotFFTazimuth(np.mean(adc_data[:, :8, 0], axis=0))
+# elev_data = np.concatenate([(np.mean(adc_data[:, :4, 0], axis=0) +
+#                              np.mean(adc_data[:, 4:8, 0], axis=0))/2, np.mean(adc_data[:, 8:, 0], axis=0)])
+# plotFFTelevation(elev_data)
 # matrix = heatmaps.plotRangeHeatmap(adc_data)
 # matrix = heatmaps.plotDopplerRangeHeatmap(adc_data)
-matrix = heatmaps.plotAzimuthRangeHeatmap(adc_data)
+# matrix = heatmaps.plotAzimuthRangeHeatmap(adc_data)
+# matrix = heatmaps.plotElevationRangeHeatmap(adc_data)