3ee860a82d93c5f79b539d9b4da15506eefd5b28
jose-glitch <pamojoan@gmail.com>
2023-01-23 15:21:47 +0000

    update with new functions

--- use_example.py
+++ use_example.py
@@ -17,42 +17,44 @@ Usage:
 import pickle
 import numpy as np
 import heatmaps
-from animated_plots import animateFFTRange, animateRangeHeatmap, animateDopplerRangeHeatmap, animateAzimuthRangeHeatmap
-from fourier import plotFFTazimuth, plotFFTelevation, plotFFTrange
+# from animated_plots import animateFFTRange, animateRangeHeatmap, animateDopplerRangeHeatmap, animateAzimuthRangeHeatmap
+from fourier import rangeFFT, angleFFT
 from params import PARAMS
 from raw_signal import RadarData, plot_signal
+import matplotlib.pyplot as plt
 
-data = pickle.load(open('data\openradar_11-01-23_PWCWPFDVFC.dat', 'rb'))
-
+# Load data from file
+data = pickle.load(open('data\openradar_12-01-23_SCRQO.dat', 'rb'))
+# Configure playback mode
 config = data['config']
-
 PARAMS.set_playback_mode(config)
-
-rdata = RadarData(device='IWR1843',
+# Read main parameters
+rdata = RadarData(device='IWR6843ISK-ODS',
                   tx=PARAMS.TX_ANTENNAS,
                   rx=PARAMS.RX_ANTENNAS,
                   loops=PARAMS.CHIRP_LOOPS,
                   samples=PARAMS.ADC_SAMPLES)
 
-# Get raw data frame
+# Get data frame
 frames = data['data']
-adc_data = frames[50]
+# Choose frame
+adc_data = frames[5]
 # Set RadarData raw_data
 rdata.raw_data = adc_data
 # Access data separated by Rx and Tx antennas
-separated = rdata.separated_data
-# Alternatively, access data separated by Vx antennas
-separated_vx = rdata.separated_vx_data
+v_array = rdata.separated_vx_data
+# 1D RANGE FFT and radar cube
+[RC, rFFT, rBins] = rangeFFT(v_array[1,:,:],rdata.device)
+# The range profile can be extracted from this as
+plt.plot(rBins[8:],np.mean(abs(rFFT),axis=0)[8:])
+plt.show()
+# 2D AZIMUTH & ELEVATION FFTs
+[aFFT, eFFT, aBins, eBins] = angleFFT(RC)
+heatmaps.plotAzimuthRangeHeatmap(rBins[:60],aBins[2:],aFFT[2:,:60])
 
-plot_signal(separated_vx[0, 0])
-# The following lines produce the same result
-# plotFFTrange(separated_vx[0, 0], PEAK_TH=90)
-# plotFFTrange(separated[0, 0, 0], PEAK_TH=90)
-# matrix = heatmaps.plotRangeHeatmap(separated_vx)
-# matrix = heatmaps.plotDopplerRangeHeatmap(separated_vx)
-# matrix = heatmaps.plotAzimuthRangeHeatmap(separated_vx)
-# matrix = heatmaps.plotElevationRangeHeatmap(separated_vx)
-# animateFFTRange(frames, PEAK_TH=95)
-# animateRangeHeatmap(frames)
-# animateDopplerRangeHeatmap(frames)
-# animateAzimuthRangeHeatmap(frames)
+# TODO Cartesian HEATMAP
+# heatmaps.plotXYheatmap(rBins,aBins,aFFT)
+
+heatmaps.plotElevationRangeHeatmap(rBins,eBins,eFFT)
+
+
